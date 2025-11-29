03f4ded8289a3570e93cc428dac36ccd4e0430ae
Istaitih <ahmadistaitih@gmail.com>
2025-11-29 02:48:40 +0200

    last

--- Branch Predictor/bp.cpp
+++ Branch Predictor/bp.cpp
@@ -1,5 +1,6 @@
 /* 046267 Computer Architecture - HW #1                                 */
 /* This file should hold your implementation of the predictor simulator */
+/*Istaitih and Jawabra*/
 
 #include <vector>
 #include "bp_api.h"
@@ -7,22 +8,22 @@
 
 using  namespace std;
 
-typedef enum{
+typedef enum{ //local or shared
 	not_using_shared =0, 
 	using_share_lsb = 1, 
 	using_share_mid =2
 } savedShared;
 
-enum state{
+enum state{ //biomodal states
 	STRONGLY_NOT_TAKEN =0,
 	WEAKLY_NOT_TAKEN =1,
 	WEAKLY_TAKEN =2,
 	STRONGLY_TAKEN =3
 };
-unsigned power(unsigned size)
+unsigned power(unsigned HistorySize)
 {
     unsigned res = 1;
-    for (unsigned i = 0; i < size; ++i) {
+    for (unsigned i = 0; i < HistorySize; ++i) {
         res *= 2;
     }
     return res;
@@ -47,23 +48,20 @@ class BTB{
 };
 
 BTB bp;
-uint32_t bitWiseXOR(uint32_t pc)
+uint32_t bitXOR(uint32_t pc)
 {
-    uint32_t temp=0;
-    //if the branch is there we need to use direct mapping using xor the lsb or the middle bits if the table is global
+    uint32_t x=0;
     if(bp.m_isGlobalTable)
     {
-        if(bp.m_Shared==using_share_lsb)
+        if(bp.m_Shared==using_share_lsb) // get the lowest bits from the pc
         {
-            //get the lowest historySize  bits from the pc
-            temp=(pc>>2)% power(bp.m_historySize);
-            return temp;
+            x=(pc>>2)% power(bp.m_historySize);
+            return x;
         }
-        if(bp.m_Shared==using_share_mid)
+        if(bp.m_Shared==using_share_mid) //get the middle bits from the pc
         {
-            //get the middle h bits from the 16 bits
-            temp=(pc>>16)% power(bp.m_historySize);
-            return temp;
+            x=(pc>>16)% power(bp.m_historySize);
+            return x;
         }
     }
     return 0;
@@ -107,13 +105,13 @@ int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned f
 	bp.m_simStats = {0,0,0};
 
 	if(isGlobalHist){
-		bp.m_history = vector<unsigned>(1,0);
+		bp.m_history = vector<unsigned>(1,0); //global history table so one entry
 	}
 	else{
 		bp.m_history = vector<unsigned>(btbSize,0);
 	}
 	if(isGlobalTable){
-		bp.m_globalTable = vector<state>(power(historySize),state(fsmState));
+		bp.m_globalTable = vector<state>(power(historySize),state(fsmState)); //initialize with fsmState
 	}
 	else{
 		bp.m_localTable = vector<vector<state>>(btbSize,vector<state>(power(historySize),state(fsmState)));
@@ -125,31 +123,31 @@ bool BP_predict(uint32_t pc, uint32_t *dst){
 	uint32_t pcIndex = (pc >> 2) % bp.m_btbSize;
 	uint32_t pcTag = ((pc >> 2)/bp.m_btbSize) % power(bp.m_tagSize);
 
-	if(bp.m_branchExists[pcIndex] == false || bp.m_tags[pcIndex] != pcTag){
+	if(bp.m_branchExists[pcIndex] == false || bp.m_tags[pcIndex] != pcTag){ // not found in btb or tag mismatch
 		*dst = pc+4;
 		return false;
 	}
 
-	uint32_t xored = bitWiseXOR(pc);
+	uint32_t xored = bitXOR(pc); //value to xor with history
 
 	if(bp.m_isGlobalTable && bp.m_isGlobalHist){
 		if(bp.m_globalTable[bp.m_history[0] ^ xored] == STRONGLY_TAKEN || bp.m_globalTable[bp.m_history[0] ^ xored] == WEAKLY_TAKEN){
-			*dst = bp.m_destination[pcIndex];
+			*dst = bp.m_destination[pcIndex]; //predicted taken
 			return true;
 		}
 		else{
-			*dst = pc+4;
+			*dst = pc+4; //predicted not taken  
 			return false;
 		}
 	}
 
 	if(bp.m_isGlobalHist){
 		if(bp.m_localTable[pcIndex][bp.m_history[0]] == STRONGLY_TAKEN || bp.m_localTable[pcIndex][bp.m_history[0]] == WEAKLY_TAKEN){
-			*dst = bp.m_destination[pcIndex];
+			*dst = bp.m_destination[pcIndex]; //predicted taken
 			return true;
 		}
 		else{
-			*dst = pc+4;
+			*dst = pc+4; //predicted not taken
 			return false;
 		}
 	}
@@ -174,84 +172,79 @@ bool BP_predict(uint32_t pc, uint32_t *dst){
 }
 
 void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst){
-    bp.m_simStats.br_num++;
-    if(taken && targetPc!=pred_dst)
+// update the predictor state for each state
+    bp.m_simStats.br_num++; // increment number of branches
+    if((taken && targetPc!=pred_dst) || (!taken && pred_dst!=pc+4)) // mispredicted
     {
-        bp.m_simStats.flush_num++;
-    }
-    if(!taken && pred_dst!=pc+4)
-    {
-        bp.m_simStats.flush_num++;
+        bp.m_simStats.flush_num++; // increment number of flushes
     }
 
-    // update now
-    uint32_t pcIndex= (pc>>2) %(bp.m_btbSize);
+    uint32_t pcIndex= (pc>>2) %(bp.m_btbSize); // index in the btb
     uint32_t pcTag=((pc>>2)/(bp.m_btbSize))% power(bp.m_tagSize);
-    bp.m_branchExists[pcIndex] = true;
-    bp.m_destination[pcIndex] = targetPc ;
+    bp.m_destination[pcIndex] = targetPc ; // update the destination
+    bp.m_branchExists[pcIndex] = true; // mark that this entry is valid
+    
 
-   // check if we need to update the history or the table to default 	
-    if(bp.m_tags[pcIndex] != pcTag){
-        if(!bp.m_isGlobalHist){
-            bp.m_history[pcIndex] = 0 ;
-        }
-
+    if(bp.m_tags[pcIndex] != pcTag){ // new entry , need to initialize
         if(!bp.m_isGlobalTable){
             bp.m_localTable[pcIndex] = vector<state>(power(bp.m_historySize),state(bp.m_state));
+        }
+        if(!bp.m_isGlobalHist){
+            bp.m_history[pcIndex] = 0 ;
         }
         bp.m_tags[pcIndex] = pcTag ;
 
     }
 
-    if(bp.m_isGlobalHist && !bp.m_isGlobalTable )
+    if((bp.m_isGlobalHist) && !(bp.m_isGlobalTable) ) // global history and local table
     {
         if(bp.m_localTable[pcIndex][bp.m_history[0]]!= STRONGLY_NOT_TAKEN && !taken )
         {
-            int ToDec =(int) bp.m_localTable[pcIndex][bp.m_history[0]] ;
-            ToDec -- ;
-            bp.m_localTable[pcIndex][bp.m_history[0]] = (state)ToDec ;
+            int Decrement =(int) bp.m_localTable[pcIndex][bp.m_history[0]] ;
+            Decrement -- ;
+            bp.m_localTable[pcIndex][bp.m_history[0]] = (state)Decrement ;
         }
         if(bp.m_localTable[pcIndex][bp.m_history[0]]!= STRONGLY_TAKEN && taken )
         {
-            int ToAdd =(int) bp.m_localTable[pcIndex][bp.m_history[0]] ;
-            ToAdd ++ ;
-            bp.m_localTable[pcIndex][bp.m_history[0]] = (state)ToAdd ;
+            int sum =(int) bp.m_localTable[pcIndex][bp.m_history[0]] ;
+            sum ++ ;
+            bp.m_localTable[pcIndex][bp.m_history[0]] = (state)sum ;
         }
 
-        bp.m_history[0] = (bp.m_history[0]*2) % power(bp.m_historySize  )  + taken ;
+        bp.m_history[0] = (bp.m_history[0]*2) % power(bp.m_historySize  )  + taken ; // update global history
 
     }
 
-    if(!bp.m_isGlobalHist && !bp.m_isGlobalTable)
+    if(!bp.m_isGlobalHist && !bp.m_isGlobalTable) // local history and local table
         {
-        if (bp.m_localTable[pcIndex][bp.m_history[pcIndex]] != STRONGLY_TAKEN && taken) {
-            int ToAdd = (int) bp.m_localTable[pcIndex][bp.m_history[pcIndex]];
-            ToAdd++;
-            bp.m_localTable[pcIndex][bp.m_history[pcIndex]] = (state) ToAdd;
-        }
         if (bp.m_localTable[pcIndex][bp.m_history[pcIndex]] != STRONGLY_NOT_TAKEN && !taken) {
-            int ToDec = (int) bp.m_localTable[pcIndex][bp.m_history[pcIndex]];
-            ToDec--;
-            bp.m_localTable[pcIndex][bp.m_history[pcIndex]] = (state) ToDec;
+            int Decrement = (int) bp.m_localTable[pcIndex][bp.m_history[pcIndex]];
+            Decrement--;
+            bp.m_localTable[pcIndex][bp.m_history[pcIndex]] = (state) Decrement;
+        }
+
+        if (bp.m_localTable[pcIndex][bp.m_history[pcIndex]] != STRONGLY_TAKEN && taken) {
+            int sum = (int) bp.m_localTable[pcIndex][bp.m_history[pcIndex]];
+            sum++;
+            bp.m_localTable[pcIndex][bp.m_history[pcIndex]] = (state) sum;
         }
 
         bp.m_history[pcIndex] =(bp.m_history[pcIndex] * 2) % power(bp.m_historySize ) + taken;
         }
-        uint32_t usedToXor= bitWiseXOR(pc);
-    /// GlobalTable
+        uint32_t Xored= bitXOR(pc);
     if(bp.m_isGlobalHist && bp.m_isGlobalTable)
     {
-      if(bp.m_globalTable[bp.m_history[0]^ usedToXor] != STRONGLY_NOT_TAKEN && !taken)
+      if(bp.m_globalTable[bp.m_history[0]^ Xored] != STRONGLY_NOT_TAKEN && !taken)
       {
-          int ToDec =(int) bp.m_globalTable[bp.m_history[0] ^ usedToXor ] ;
-          ToDec -- ;
-          bp.m_globalTable[bp.m_history[0]^ usedToXor ] = (state)ToDec ;
+          int Decrement =(int) bp.m_globalTable[bp.m_history[0] ^ Xored ] ;
+          Decrement -- ;
+          bp.m_globalTable[bp.m_history[0]^ Xored ] = (state)Decrement ;
       }
-        if(bp.m_globalTable[bp.m_history[0]^ usedToXor] != STRONGLY_TAKEN && taken)
+        if(bp.m_globalTable[bp.m_history[0]^ Xored] != STRONGLY_TAKEN && taken)
         {
-            int ToAdd =(int) bp.m_globalTable[bp.m_history[0] ^ usedToXor] ;
-            ToAdd ++ ;
-            bp.m_globalTable[bp.m_history[0] ^ usedToXor ] = (state)ToAdd ;
+            int sum =(int) bp.m_globalTable[bp.m_history[0] ^ Xored] ;
+            sum ++ ;
+            bp.m_globalTable[bp.m_history[0] ^ Xored ] = (state)sum ;
         }
 
         bp.m_history[0] = ((bp.m_history[0]*2) % power(bp.m_historySize ) ) + taken ;
@@ -260,19 +253,19 @@ void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst){
 
     if(!bp.m_isGlobalHist && bp.m_isGlobalTable)
     {
-      if(bp.m_globalTable[bp.m_history[pcIndex] ^ usedToXor]!= STRONGLY_TAKEN && taken )
-      {
-          int ToAdd =(int) bp.m_globalTable[bp.m_history[pcIndex] ^ usedToXor] ;
-          ToAdd ++ ;
-          bp.m_globalTable[bp.m_history[pcIndex] ^ usedToXor] = (state)ToAdd ;
-      }
 
-      if(bp.m_globalTable[bp.m_history[pcIndex] ^ usedToXor]!= STRONGLY_NOT_TAKEN && !taken )
+      if(bp.m_globalTable[bp.m_history[pcIndex] ^ Xored]!= STRONGLY_NOT_TAKEN && !taken )
         {
-            int ToDec =(int) bp.m_globalTable[bp.m_history[pcIndex] ^ usedToXor] ;
-            ToDec -- ;
-            bp.m_globalTable[bp.m_history[pcIndex] ^ usedToXor] = (state)ToDec ;
+            int Decrement =(int) bp.m_globalTable[bp.m_history[pcIndex] ^ Xored] ;
+            Decrement -- ;
+            bp.m_globalTable[bp.m_history[pcIndex] ^ Xored] = (state)Decrement ;
         }
+      if(bp.m_globalTable[bp.m_history[pcIndex] ^ Xored]!= STRONGLY_TAKEN && taken )
+      {
+          int sum =(int) bp.m_globalTable[bp.m_history[pcIndex] ^ Xored] ;
+          sum ++ ;
+          bp.m_globalTable[bp.m_history[pcIndex] ^ Xored] = (state)sum ;
+      }
 
         bp.m_history[pcIndex] =(bp.m_history[pcIndex] * 2) % power(bp.m_historySize ) + taken;
 
@@ -280,39 +273,29 @@ void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst){
 
 }
 
-
-// calculate the btb size 
-void getBTBSize()
-{
-   if(bp.m_isGlobalTable && bp.m_isGlobalHist)
+void BP_GetStats(SIM_stats *curStats){ 
+    // calculate the size
+    if(bp.m_isGlobalTable && bp.m_isGlobalHist)
     {
         bp.m_simStats.size=bp.m_btbSize * (bp.m_tagSize + 1 + 30);
         bp.m_simStats.size+= 2*power(bp.m_historySize) + bp.m_historySize;
-        return;
     }
     else if(bp.m_isGlobalHist && !bp.m_isGlobalTable)
     {
         bp.m_simStats.size=bp.m_historySize;
         bp.m_simStats.size+= bp.m_btbSize* (bp.m_tagSize + 1 + 30 + 2*power(bp.m_historySize));
-        return;
     }
     else if(!bp.m_isGlobalHist  && bp.m_isGlobalTable)
     {
         bp.m_simStats.size=2 * power(bp.m_historySize);
         bp.m_simStats.size+= bp.m_btbSize * (bp.m_tagSize + bp.m_historySize + 1 + 30);
-        return;
     }
     else {
         bp.m_simStats.size = (2 * power(bp.m_historySize)) + 1 + 30 + bp.m_tagSize +
                                   bp.m_historySize;
         bp.m_simStats.size = bp.m_btbSize * bp.m_simStats.size;
     }
-
-}
-
-
-void BP_GetStats(SIM_stats *curStats){
-    getBTBSize();
+    // set the stats
     curStats->size = bp.m_simStats.size;
     curStats->flush_num= bp.m_simStats.flush_num;
     curStats->br_num=bp.m_simStats.br_num;
